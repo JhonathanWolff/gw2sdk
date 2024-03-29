1b1292fe9709ddb76a836a3ffdd74882bbe4b8e5
DotFiles <dotfiles@myemail.com>
2024-03-29 17:47:46 -0300

    update account resource

--- authenticated/account/account.go
+++ authenticated/account/account.go
@@ -1,4 +1,4 @@
-package account
+package authenticated
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"gw2sdk/achivements"
+	authenticated "gw2sdk/authenticated/account"
 	"gw2sdk/connection"
 	"os"
 )
 
 func main() {
 	var gw2sdk connection.GW2sdk = connection.GW2sdk{Token: os.Getenv("GW2_TOKEN")}
-	var achivement = achivements.Category{Gw2sdk: gw2sdk}
-	var parameter map[string]string = make(map[string]string)
-	parameter["ids"] = "1,2"
+	account := &authenticated.Account{Gw2sdk: gw2sdk}
+	// var parameter map[string]string = make(map[string]string)
+	// parameter["ids"] = "1,2"
 
-	//parameter := "18DB115A-8637-4290-A636-821362A3C4A8"
-
-	fmt.Println(achivement.Get(parameter))
+	fmt.Println(account.Get())
 
 }
